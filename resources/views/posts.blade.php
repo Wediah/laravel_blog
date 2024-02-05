d53cc3d5d69457fe55867f1bd7167b4eca17bd51
Wediah <100675006+Wediah@users.noreply.github.com>
2024-02-05 15:09:29 +0000

    added various posts pages

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="app.css">
+
+<body>
+    <article>
+        <a href="/post"><h1>My First Post</h1></a>
+
+        <p>
+            All of the configuration files for the Laravel framework are stored in the config directory. Each option
+            is documented, so feel free to look through the files and get familiar with the options available to you.
+            Laravel needs almost no additional configuration out of the box. You are free to get started developing! However, you may wish to review the config/app.php file and its documentation. It contains several options such as timezone and locale that you may wish to change according to your application.
+        </p>
+    </article>
+
+
+    <article>
+        <h1>My Second Post</h1>
+
+        <p>
+            All of the configuration files for the Laravel framework are stored in the config directory. Each option
+            is documented, so feel free to look through the files and get familiar with the options available to you.
+            Laravel needs almost no additional configuration out of the box. You are free to get started developing! However, you may wish to review the config/app.php file and its documentation. It contains several options such as timezone and locale that you may wish to change according to your application.
+        </p>
+    </article>
+
+
+    <article>
+        <h1>My Third Post</h1>
+
+        <p>
+            All of the configuration files for the Laravel framework are stored in the config directory. Each option
+            is documented, so feel free to look through the files and get familiar with the options available to you.
+            Laravel needs almost no additional configuration out of the box. You are free to get started developing! However, you may wish to review the config/app.php file and its documentation. It contains several options such as timezone and locale that you may wish to change according to your application.
+        </p>
+    </article>
+</body>

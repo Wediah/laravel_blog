fbfd2e335a1f4693dfa4f5d464544f393220fffe
Wediah <100675006+Wediah@users.noreply.github.com>
2024-02-22 22:36:11 +0000

    comments out posts page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,18 +4,18 @@
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         <x-post-featured-card />
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
-        </div>
+{{--        <div class="lg:grid lg:grid-cols-2">--}}
+{{--            <x-post-card />--}}
+{{--            <x-post-card />--}}
+{{--        </div>--}}
 
-        <div class="lg:grid lg:grid-cols-3">
+{{--        <div class="lg:grid lg:grid-cols-3">--}}
 
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
+{{--            <x-post-card />--}}
+{{--            <x-post-card />--}}
+{{--            <x-post-card />--}}
 
-        </div>
+{{--        </div>--}}
     </main>
 {{--    @foreach ($posts as $post)--}}
 {{--        <article>--}}

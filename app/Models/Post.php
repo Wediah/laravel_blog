17e94e12d0e7cff6ccc3b82d7657b9f789d4df51
Wediah <100675006+Wediah@users.noreply.github.com>
2024-02-27 15:21:52 +0000

    implements scopes for search and also using the query builder to make simple queries

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -25,6 +25,13 @@ class Post extends Model
 //        return 'slug';
 //    }
 
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, fn ($query, $search) =>
+            $query->where('title', 'like', '%' . $search .'%')
+                ->orWhere('body', 'like', '%' . $search .'%'));
+    }
+
 
     public function category()
     {
--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -57,8 +57,12 @@
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET" action="#">
-                <input type="text" name="search" placeholder="Find something"
-                       class="bg-transparent placeholder-black font-semibold text-sm">
+                <input
+                    type="text" name="search"
+                    placeholder="Find something"
+                    class="bg-transparent placeholder-black font-semibold text-sm border-0 outline-none"
+                    value="{{ request('search') }}"
+                >
             </form>
         </div>
     </div>

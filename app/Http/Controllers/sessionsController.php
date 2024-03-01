2b14efc522fba7fbad97e324160e3142d9c12440
Wediah <100675006+Wediah@users.noreply.github.com>
2024-03-01 01:10:22 +0000

    builds login page

--- app/Http/Controllers/sessionsController.php
+++ app/Http/Controllers/sessionsController.php
@@ -3,9 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Nette\Schema\ValidationException;
 
 class sessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            session()->regenerate();
+            return redirect('/')->with('success', 'Welcome Back');
+        }
+
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified'
+        ]);
+    }
     public function destroy()
     {
         auth()->logout();

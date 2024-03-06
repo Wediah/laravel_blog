e5aaecd592dbc0ce8495b87f50a8e5eb5067eb27
Wediah <100675006+Wediah@users.noreply.github.com>
2024-03-06 15:03:07 +0000

    makes comment section dynamic

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,21 +1,22 @@
+@props(['comment'])
+
 <article class="flex gap-2 bg-gray-100 p-6 rounded-xl border border-gray-200">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/100" alt="" class="rounded-xl">
+        <img src="https://i.pravatar.cc/100?u={{ $comment->id }}" alt="" class="rounded-xl">
     </div>
 
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">John Loo</h3>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
             <p class="text-xs">
                 <time>
-                    Posted 8 months ago
+                    {{ $comment->created_at->diffForHumans() }}
                 </time>
             </p>
         </header>
 
         <p>
-            It is a long-established fact that a reader will be distracted by the readable content of
-            a page when looking at its layout.
+            {{ $comment->body }}
         </p>
     </div>
 </article>

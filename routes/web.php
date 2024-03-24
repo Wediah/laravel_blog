7bf963eb84ee53e0e0a115cf4ab8d788e43c1637
Wediah <100675006+Wediah@users.noreply.github.com>
2024-03-24 23:32:17 +0000

    extracts newsletter service

--- routes/web.php
+++ routes/web.php
@@ -1,40 +1,21 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\sessionsController;
 use App\Models\Category;
+use App\Services\Newsletter;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use Illuminate\Validation\ValidationException;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 use App\Models\User;
 
-Route::post('newsletter', function () {
-    request()->validate(['email' => 'required|email']);
-
-    $mailchimp = new \MailchimpMarketing\ApiClient();
-
-    $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => 'us18'
-    ]);
-
-    try {
-        $response = $mailchimp->lists->addListMember('d544f2ffdc', [
-            'email_address' => request('email'),
-            'status' => 'subscribed'
-        ]);
-    } catch (\Exception $e) {
-        throw \Illuminate\Validation\ValidationException::withMessages([
-            'email' => 'This email could not be added to our newsletter list.'
-        ]);
-    }
-
-    return redirect('/')->with('success', 'You are now subscribed for our newsletter.');
-});
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);

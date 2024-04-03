f48321702b673bff5a9c4862aeca2902bf89c2b3
Wediah <100675006+Wediah@users.noreply.github.com>
2024-04-03 22:29:17 +0000

    fixes 404 error

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-setting :heading="'Edit Post: ' . $post->title">
         <form method="POST"
-              action="/admin/posts{{ $post->id }}"
+              action="/admin/posts/{{ $post->id }}"
               enctype="multipart/form-data"
         >
             @csrf
